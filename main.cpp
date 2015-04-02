8f2176bff717050982ea4f9767dba13d0fb12af1
AnaSteele <marine.taliercio@sfr.fr>
2015-04-02 17:52:49 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ *  Fichier     main.cpp
+ *  Dossier     Mini projet "Avalam"
+ *  Auteur      Marine Talier
+ *  date        Date de début du projet
+ *  
+ *  Fichier main du projet
+ */
+
+
+#include <iostream>
+#include <string>
+#include "Plateau.h"
+#include "Menu.h"
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+
+int main() {
+
+    Menu m;
+    m.lancerPartie();
+
+    
+    return 0;
+}
