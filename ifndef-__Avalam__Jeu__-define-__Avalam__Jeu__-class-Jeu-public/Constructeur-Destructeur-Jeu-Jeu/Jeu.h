d3f0fb993eef19149babc229df7a16eb6089c7fe
AnaSteele <marine.taliercio@sfr.fr>
2015-05-06 12:15:20 +0200

    Create Jeu.h

---
+++ ifndef-__Avalam__Jeu__-define-__Avalam__Jeu__-class-Jeu-public/Constructeur-Destructeur-Jeu-Jeu/Jeu.h
@@ -0,0 +1,15 @@
+#ifndef __Avalam__Jeu__
+#define __Avalam__Jeu__
+
+class Jeu{
+public:
+    
+    // Constructeur & Destructeur
+    Jeu();
+    ~Jeu();
+    
+    //
+    void lancer();
+};
+
+#endif
