004554ce44a8f1aef7302a0ee744ed86f96f6783
AnaSteele <marine.taliercio@sfr.fr>
2015-05-06 12:14:01 +0200

    Create Coup.h

---
+++ Coup.h
@@ -0,0 +1,16 @@
+#ifndef __Avalam__Coup__
+#define __Avalam__Coup__
+
+#include "Case.h"
+
+class Coup{
+public:
+    Coup();
+    ~Coup();
+    
+private:
+    Case Depart;
+    Case Arrivee;
+};
+
+#endif
