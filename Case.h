3e1d239bf9c02604249249a23ca2436401ca1eb7
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:40:40 +0200

    Update Case.h

--- Case.h
+++ Case.h
@@ -1,22 +1,40 @@
+/*
+ * Fichier      Case.h
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
+ */
 
 #ifndef __Avalam__Case__
 #define __Avalam__Case__
 
+const char tourRouge = 'R';
+const char tourBleu= 'B';
+const char caseInterdite= 'X';
+const char caseVide= ' ';
+
 class Case{
+private:
+    unsigned int ligne;
+    unsigned int colonne;
+    unsigned int compteur;
+    char tour;
+
 public:
     Case();
+    Case(unsigned int l, unsigned int c, char t, unsigned int cpt);
     ~Case();
-    Case(int l, int c, char t, int cpt);
+    
     char getTour();
+    
+    void setTour(char t);
+    void setLigne(unsigned int);
+    void setColonne(unsigned int);
+    void setCompteur(unsigned int);
+    
     int getLigne();
     int getColonne();
     int getCompteur();
-    
-private:
-    int ligne;
-    int colonne;
-    char tour;
-    int compteur;
 };
 
 #endif
