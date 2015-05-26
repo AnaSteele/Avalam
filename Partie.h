39fb078f49c73bd5738b708d77e42642f0a36939
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:42:47 +0200

    Update Partie.h

--- Partie.h
+++ Partie.h
@@ -1,30 +1,31 @@
-
 /*
- * Inclusions
+ * Fichier      Partie.h
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
  */
 
 #ifndef __Avalam__Partie__
 #define __Avalam__Partie__
 
 #include "Joueur.h"
-#include "Coup.h"
 
 class Partie{
-public:
+private:
+    bool tourJoueur;
+    bool quitter;
     
-    // Constructeur & Destructeur
+    int compteur;
+    
+public:
     Partie(int i);
     ~Partie();
-    
-    //
+
     int jouerPartie();
-    void ajoutCoup(Coup c);
-    void afficherTabCoups();
-    void affichageScoreJoueur(Joueur J1, Joueur J2); 
     
-private:
-    Coup* tabCoups;
-    bool tourJoueur; 
+    void affichageScoreJoueur(Joueur J1, Joueur J2);
+    
+    bool aQuiDeJouer();
 };
 
 #endif
