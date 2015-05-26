442375d4b921562f4a49a248e08a444addeaf36c
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:43:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include "Jeu.h"
-#include "Joueur.h"
+/*
+ * Fichier      main.cpp
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
+ */
 
-int main() {
+#include "Jeu.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "**************************************" << endl;
+    cout << "*              BIENVENUE             *" << endl;
+    cout << "*                                    *" << endl;
+    cout << "*               Avalam               *" << endl;
+    cout << "*    Réalisé par Marine TALIERCIO    *" << endl;
+    cout << "**************************************" << endl;
     
     Jeu avalam;
     avalam.lancer();
