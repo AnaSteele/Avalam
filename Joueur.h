f5d07bbfb82937eb7b69845a31ce5dabb6b57634
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:42:16 +0200

    Update Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -1,6 +1,8 @@
-
 /*
- * Inclusions
+ * Fichier      Joueur.h
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
  */
 
 #ifndef __Avalam__Joueur__
@@ -8,21 +10,33 @@
 
 #include <string>
 
+// TYPE DE JOUEUR 
 //enum class typePlayer {Humain, Ordi};
 
 class Joueur{
+private:
+    std::string nom;
+    int id;
+    int score;
+    char couleur;
+
 public:
     Joueur();
-    Joueur(int id, std::string nom, int score);
-    std::string getNom() const;
-    int getId() const;
-    void saisieNomJoueur();
+    Joueur(int jId, std::string jNom, int jScore, char jCouleur);
     
-private:
-    std::string m_name;
-    int m_id;
-    int m_score;
+    std::string getNom();
+    
+    int getId();
+    
+    char getCouleur();
+    
+    void saisieNomJoueur();
+    void choixCouleurTour();
+    void setTour(char couleur);
+    void setNom(std::string nom);
 };
+
+
 
 
 
