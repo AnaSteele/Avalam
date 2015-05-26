b130254e0cd9a2d3b19c8d89d185045a0b918f56
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:41:45 +0200

    Update Jeu.h

--- Jeu.h
+++ Jeu.h
@@ -1,14 +1,18 @@
+/*
+ * Fichier      Jeu.h
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
+ */
+
 #ifndef __Avalam__Jeu__
 #define __Avalam__Jeu__
 
 class Jeu{
 public:
-    
-    // Constructeur & Destructeur
     Jeu();
     ~Jeu();
-    
-    //
+
     void lancer();
 };
 
