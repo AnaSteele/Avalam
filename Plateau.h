79ac84a41999446fd8d7ed90f1f4f94c394a05ca
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:43:26 +0200

    Update Plateau.h

--- Plateau.h
+++ Plateau.h
@@ -1,33 +1,77 @@
+/*
+ * Fichier      Plateau.h
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
+ */
+
+
 #ifndef __Avalam__Plateau__
 #define __Avalam__Plateau__
 
-//#include "Case.h"
+#include "Case.h"
 #include "Joueur.h"
 
-const char pionRouge = 'R';
-const char pionBleu= 'B';
-const char caseInterdite= 'X';
-const char caseVide= ' ';
-
 class Plateau{
-public:
+private:
+    Case** plateau;
     
-    // Constructeur & Destructeur
+    Case Depart;
+    Case Arrivee;
+    
+    unsigned int nombreDeLignes;
+    unsigned int nombreDeColonnes;
+    
+    char couleur;
+    
+    int compteurMax;
+    int ligne;
+    int colonne; 
+    
+    bool diagonaleBasDroite;
+    bool diagonaleBasGauche;
+    bool diagonaleHautDroite;
+    bool diagonaleHautGauche;
+    bool haut;
+    bool droite;
+    bool gauche;
+    bool bas;
+    bool tourPleine;
+    bool tourNonPleine;
+    bool finPartie;
+    bool victoirePartie;
+    bool test;
+    
+    
+public:
     Plateau();
     Plateau(unsigned int lignes, unsigned int colonnes);
     ~Plateau();
     
-    
-    //
     void lectureFichier();
     void afficherPlateau();
-    int scoreJoueur(char couleur);
-    void affichageScoreJoueur(Joueur J1, Joueur J2);
+    void demandeDeplacement();
+    void deplacementPossible();
+    void effectuerDeplacement(char couleur,Case Depart, Case Arrivee);
+    void setCompteurMax(unsigned int  cptMax);
     
-private:
-    char** plateau;
-    unsigned int nombreDeLignes;
-    unsigned int nombreDeColonnes;
+    bool testDepart();
+    bool testArrivee();
+    bool testDiagonaleBasDroite();
+    bool testDiagonaleBasGauche();
+    bool testDiagonaleHautGauche();
+    bool testDiagonaleHautDroite();
+    bool testBas();
+    bool testHaut();
+    bool testDroite();
+    bool testGauche();
+    bool estUneTourNonPleine(unsigned int ligne, unsigned int colonne);
+    bool estUneTourPleine(unsigned int ligne, unsigned int colonne);
+    bool testSiFinPartie();
+    bool victoire();
+    
+    int scoreJoueur(char couleur);
+    int getCompteurMax();
 };
 
 #endif 
