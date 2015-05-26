1896a7ad49d2a1b6ab400a603a8f20d245f63bea
AnaSteele <marine.taliercio@sfr.fr>
2015-05-26 11:41:26 +0200

    Update Jeu.cpp

--- Jeu.cpp
+++ Jeu.cpp
@@ -1,6 +1,8 @@
-
 /*
- * Inclusions
+ * Fichier      Jeu.cpp
+ *              Mini-Projet "Avalam"
+ * Auteur       Marine Taliercio
+ * Date         2015
  */
 
 #include <iostream>
@@ -10,37 +12,47 @@
 
 using namespace std; 
 
+/*
+ * Fonction     Jeu()
+ * Description  Methode constructeur d'un jeu
+ * Arguments    Auncun
+ * Renvoi       Rien
+ */
+
 Jeu::Jeu(){
     
 }
+
+/*
+ * Fonction     Jeu()
+ * Description  Methode destructeur d'un jeu
+ * Arguments    Auncun
+ * Renvoi       Rien
+ */
 
 Jeu::~Jeu(){
     
 }
 
+/*
+ * Fonction     lancer()
+ * Description  Fonction qui lance le jeu Avalam
+ * Arguments    Auncun
+ * Renvoi       Rien
+ */
+
 void Jeu::lancer(){
-    
-    /*int continuer=0;
-    while(continuer==0){
-        Partie P(1);
-        continuer=P.jouerPartie();
-        cout << "voulez vous continuer ? (0/1)";
-        cin>>continuer;
-    }*/
-    
-    
     Affichage A;
     A.menuJeu();
     int choixMenu=A.lireChoixMenuJeu();
-
-        switch(choixMenu){
-            
+    switch(choixMenu){
         case 1:{
             int continuer=0;
             while(continuer==0){
                 Partie P(1);
                 continuer=P.jouerPartie();
-                cout << "voulez vous continuer ? (0/1)";
+                cout << "VOULEZ VOUS QUITTER LE JEU ?" << endl;
+                cout << "  0 - NON       1 - OUI" << endl;
                 cin>>continuer;
             }
             break;
@@ -50,7 +62,7 @@ void Jeu::lancer(){
             A.reglesDuJeu();
             char revenirMenuPrincipal;
             while(revenirMenuPrincipal!='o' || revenirMenuPrincipal!='O' || revenirMenuPrincipal!='n' || revenirMenuPrincipal!='N'){
-                cout << "Voulez vous revenir au Menu Principal ? (O-N) >> ";
+                cout << "REVENIR AU MENU PRINCIPAL ? (O-N) >> ";
                 cin >> revenirMenuPrincipal;
                 cout << endl;
                 if (revenirMenuPrincipal=='o' || revenirMenuPrincipal=='O') lancer();
@@ -61,15 +73,10 @@ void Jeu::lancer(){
         }
             
         case 3:{
-                A.parametresDuJeu();
-                break;
-            }
-            
-        case 4:{
             A.aPropos();
             char revenirMenuPrincipal;
             while(revenirMenuPrincipal!='o' || revenirMenuPrincipal!='O' || revenirMenuPrincipal!='n' || revenirMenuPrincipal!='N'){
-                cout << "Voulez vous revenir au Menu Principal ? (O-N) >> ";
+                cout << "REVENIR AU MENU PRINCIPAL ? (O-N) >> ";
                 cin >> revenirMenuPrincipal;
                 cout << endl;
                 if (revenirMenuPrincipal=='o' || revenirMenuPrincipal=='O') lancer();
@@ -79,17 +86,17 @@ void Jeu::lancer(){
             break;
         }
             
-        case 5:{
-            cout << "* QUITTER LE JEU *" << endl;
+        case 4:{
+            cout << endl;
+            cout << "AUREVOIR" << endl;
             A.quitterLeJeu();
             break;
         }
             
         default:{
-            cout << "ERREUR >> LE MENU QUE VOUS AVEZ CHOISI N'EXISTE PAS " << endl;
-            lancer(); 
+            cout << "ERREUR : LE MENU N'EXISTE PAS" << endl;
+            lancer();
             break;
         }
     }
-    
 }
